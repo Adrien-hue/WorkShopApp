351861b0220c82cc296a47b1d48b83f6b5fe0cc3
Dorian <dorian161100@hotmail.fr>
2020-06-17 12:17:06 +0200

    Explication & Nouvelle page

--- FilmAmora/app/src/main/java/com/example/filmamora/MainActivity.java
+++ FilmAmora/app/src/main/java/com/example/filmamora/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.filmamora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView helloWorld;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.helloWorld = (TextView) findViewById(R.id.helloWorld);
+        this.btn2 = (Button) findViewById(R.id.btn2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextPage = new Intent(getApplicationContext(), Page2.class);
+                startActivity(nextPage);
+                finish();
+            }
+        });
+
+
+
+
+
+
     }
 }
