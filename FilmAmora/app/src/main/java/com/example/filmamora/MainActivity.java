45148179baa09910861c3e6b38548b2972c6feea
Calibet Yann <yanncalibet@gmail.com>
2020-06-18 12:07:31 +0200

    #1
    majuscule a interstellar

--- FilmAmora/app/src/main/java/com/example/filmamora/MainActivity.java
+++ FilmAmora/app/src/main/java/com/example/filmamora/MainActivity.java
@@ -31,22 +31,22 @@ public class MainActivity extends AppCompatActivity {
         //List des items
         List <Film> listFilm = new ArrayList<Film>();
         listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
-        listFilm.add(new Film("interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
+        listFilm.add(new Film("Interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
+        listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
+        listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
+        listFilm.add(new Film("Interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
         listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
         listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
         listFilm.add(new Film("interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
         listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
         listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
-        listFilm.add(new Film("interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
+        listFilm.add(new Film("Interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
         listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
         listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
-        listFilm.add(new Film("interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
+        listFilm.add(new Film("Interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
         listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
         listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
-        listFilm.add(new Film("interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
-        listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
-        listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,"Le 26 mars 2018"));
-        listFilm.add(new Film("interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
+        listFilm.add(new Film("Interstellar", 2,"Par Christopher Nolan" ,"Le 5 novembre 2014"));
         listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", "Le 26 juillet 2017"));
 
 
