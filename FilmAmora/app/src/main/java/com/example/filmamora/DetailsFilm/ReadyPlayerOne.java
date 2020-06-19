71b45fd569adb479df7a36a8523f4ca78ab01150
Dorian <dorian161100@hotmail.fr>
2020-06-19 14:48:03 +0200

    Page détail fait

---
+++ FilmAmora/app/src/main/java/com/example/filmamora/DetailsFilm/ReadyPlayerOne.java
@@ -0,0 +1,17 @@
+package com.example.filmamora.DetailsFilm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.filmamora.R;
+
+public class ReadyPlayerOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ready_player_one);
+
+    }
+}
--- FilmAmora/app/src/main/java/com/example/filmamora/MainActivity.java
+++ FilmAmora/app/src/main/java/com/example/filmamora/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.filmamora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.example.filmamora.Adapter.FilmAdapter;
+import com.example.filmamora.DetailsFilm.ReadyPlayerOne;
 import com.example.filmamora.Objet.Film;
 import java.util.ArrayList;
 
@@ -25,16 +30,41 @@ public class MainActivity extends AppCompatActivity {
         this.filmName = findViewById(R.id.item_name);
 
         //List des items
-        ArrayList <Film> listFilm = new ArrayList<Film>();
+        final ArrayList <Film> listFilm = new ArrayList<Film>();
         listFilm.add(new Film("Ready Player One", 1, "Par Steven Spielberg" ,2018));
         listFilm.add(new Film("Interstellar", 2,"Par Christopher Nolan" ,2014));
         listFilm.add(new Film("Valérian et la cité des mille planètes",3,"Par Luc Besson", 2017));
         listFilm.add(new Film("Matrix",4,"Par les Wachowski", 1999));
         listFilm.add(new Film("Her",5,"Par Spike Jonze", 2013));
 
+
         ListView listView = findViewById(R.id.ListFilm);
 
         listView.setAdapter(new FilmAdapter(this, listFilm));
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                if(position == 0){
+                    Intent rdy = new Intent(getApplicationContext(), ReadyPlayerOne.class);
+                    startActivity(rdy);
+
+                }else if(position == 1){
+
+
+                }else if(position == 2){
+
+                }else if(position == 3){
+
+                }else if(position == 4){
+
+                }else if(position == 5){
+
+                }
+            }
+        });
 
     }
 }
