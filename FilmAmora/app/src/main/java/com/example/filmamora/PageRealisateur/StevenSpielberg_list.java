58678d6d9b10475550decd4596a94de629b75f17
Dorian <dorian161100@hotmail.fr>
2020-06-20 15:59:33 +0200

    #2
    Save

---
+++ FilmAmora/app/src/main/java/com/example/filmamora/PageRealisateur/StevenSpielberg_list.java
@@ -0,0 +1,16 @@
+package com.example.filmamora.PageRealisateur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.filmamora.R;
+
+public class StevenSpielberg_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steven_spielberg_list);
+    }
+}
